e00cc8a963e495e673421c4451bc33d18ac4127f
fabrizio <fdellagiacoma@efs.at>
2024-04-10 13:57:19 +0200

    synthax

--- cfn-lint.sh
+++ cfn-lint.sh
@@ -2,10 +2,9 @@
 # set -uxo pipefail
 set -e
 
-# pip install cfn-lint
+pip install cfn-lint
 cfn-lint template.yaml --ignore-checks W; exit_status=$?
 echo $exit_status
-
 
 ERROR_MASK=2
 WARNING_MASK=4
