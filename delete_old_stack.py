314876e2cba4ad39386fee237281699b945661fb
fabrizio <fdellagiacoma@efs.at>
2024-04-19 10:02:13 +0200

    dashboard with same name...

---
+++ delete_old_stack.py
@@ -0,0 +1,7 @@
+import boto3
+
+cf_client = boto3.client('cloudformation', region_name="eu-west-1")
+
+for stack in cf_client.describe_stacks()['Stacks']:
+    print("deleting {}".format(stack['StackName']))
+    cf_client.delete_stack(StackName=stack['StackName'])
